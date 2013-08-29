20f3db6c13007c76451ac2ed9d90702ffead4563
xiaominglui <xiaominglui@gmail.com>
2013-08-29 19:04:42 +0800

    backup commit for push sync

--- android/src/com/mili/xiaominglui/app/vello/receiver/AVOSPushReceiver.java
+++ android/src/com/mili/xiaominglui/app/vello/receiver/AVOSPushReceiver.java
@@ -9,6 +9,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 public class AVOSPushReceiver extends BroadcastReceiver {
 	private static final String TAG = AVOSPushReceiver.class.getSimpleName();
@@ -16,27 +17,22 @@ public class AVOSPushReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		String action = intent.getAction();
-		if (action.equals("com.avos.UPDATE_STATUS")) {
-			Log.d(TAG, "xxxxx");
-		}
-//		Log.d(TAG, "AVOSPushReceiver Get Broadcat, action is " + intent.getAction());
-		
-		
-//        try {
-//            String action = intent.getAction();
-//            String channel = intent.getExtras().getString("com.parse.Channel");
-//            JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
-//
-//            Log.d(TAG, "got action " + action + " on channel " + channel + " with:");
-//            Iterator itr = json.keys();
-//            while (itr.hasNext()) {
-//                String key = (String) itr.next();
-//                Log.d(TAG, "..." + key + " => " + json.getString(key));
-//            }
-//        } catch (JSONException e) {
-//            Log.d(TAG, "JSONException: " + e.getMessage());
-//        }
-		
-	}
 
+		if (action.equals("com.avos.avello.SYNC_MSG")) {
+			// String channel =
+			// intent.getExtras().getString("com.parse.Channel");
+			try {
+				JSONObject json = new JSONObject(intent.getExtras().getString(
+						"com.parse.Data"));
+				Iterator itr = json.keys();
+				while (itr.hasNext()) {
+					String key = (String) itr.next();
+					Log.d(TAG, "..." + key + " => " + json.getString(key));
+				}
+			} catch (JSONException e) {
+				Log.d(TAG, "JSONException: " + e.getMessage());
+			}
+			Toast.makeText(context, "SYNC_MSG", Toast.LENGTH_SHORT).show();
+		}
+	}
 }
