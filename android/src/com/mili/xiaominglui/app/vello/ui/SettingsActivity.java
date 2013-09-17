0ce9ad676a20d8cfa358b620c86e05e08a3975de
xiaominglui <xiaominglui@gmail.com>
2013-09-17 18:12:10 +0800

    backup commit for sync settings

--- android/src/com/mili/xiaominglui/app/vello/ui/SettingsActivity.java
+++ android/src/com/mili/xiaominglui/app/vello/ui/SettingsActivity.java
@@ -1,27 +1,125 @@
 package com.mili.xiaominglui.app.vello.ui;
 
-import com.mili.xiaominglui.app.vello.R;
+import java.lang.ref.WeakReference;
 
+import com.mili.xiaominglui.app.vello.R;
+import com.mili.xiaominglui.app.vello.authenticator.Constants;
+import com.mili.xiaominglui.app.vello.config.VelloConfig;
+import com.mili.xiaominglui.app.vello.data.provider.VelloProvider;
+import com.mili.xiaominglui.app.vello.service.VelloService;
+import com.mili.xiaominglui.app.vello.util.AccountUtils;
+
+import android.accounts.Account;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 import android.util.Log;
 
 public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	public static final String KEY_PREF_SYNC_FREQ = "pref_sync_frequency";
 	
 	private ListPreference mListPreference;
+	private SettingsActivityUIHandler mUICallback = new SettingsActivityUIHandler(this);
+	private class SettingsActivityUIHandler extends Handler {
+		WeakReference<SettingsActivity> mActivity;
+		
+		SettingsActivityUIHandler(SettingsActivity activity) {
+			mActivity = new WeakReference<SettingsActivity>(activity);
+		}
+		
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			super.handleMessage(msg);
+		}
+	}
+	Messenger mService = null;
+	private boolean mIsBound;
+
+	final Messenger mMessenger = new Messenger(mUICallback);
+
+	private ServiceConnection mConnection = new ServiceConnection() {
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			// This is called when the connection with the service has been
+			// established, giving us the service object we can use to
+			// interact with the service. Because we have bound to a explicit
+			// service that we know is running in our own process, we can
+			// cast its IBinder to a concrete class and directly access it.
+			mService = new Messenger(service);
+
+			try {
+				Message msg = Message.obtain(null,
+						VelloService.MSG_REGISTER_CLIENT);
+				msg.replyTo = mMessenger;
+				mService.send(msg);
+			} catch (RemoteException e) {
+				// In this case the service has crashed before we could even do
+				// anything with it
+			}
+		}
+
+		public void onServiceDisconnected(ComponentName className) {
+			// This is called when the connection with the service has been
+			// unexpectedly disconnected - process crashed.
+			mService = null;
+		}
+	};
+
+	private void sendMessageToService(int type) {
+		if (mIsBound) {
+			if (mService != null) {
+				try {
+					Message msg = Message.obtain(null, type);
+					msg.replyTo = mMessenger;
+					mService.send(msg);
+				} catch (RemoteException e) {
+				}
+			}
+		}
+	}
+	
+	void doBindService() {
+		// Establish a connection with the service. We use an explicit
+		// class name because we want a specific service implementation that
+		// we know will be running in our own process (and thus won't be
+		// supporting component replacement by other applications).
+		bindService(new Intent(this, VelloService.class), mConnection,
+				Context.BIND_AUTO_CREATE);
+		mIsBound = true;
+	}
+
+	void doUnbindService() {
+		if (mIsBound) {
+			// Detach our existing connection.
+			unbindService(mConnection);
+			mIsBound = false;
+		}
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
             getActionBar().setDisplayHomeAsUpEnabled(true);
         }
+		
+		doBindService();
 	}
 	
 	@Override
@@ -45,6 +143,12 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 		mListPreference = (ListPreference) getPreferenceScreen().findPreference(KEY_PREF_SYNC_FREQ);
 	}
 	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		doUnbindService();
+	}
+	
 	private void setupSimplePreferencesScreen() {
         // Add 'general' preferences.
         addPreferencesFromResource(R.xml.preferences);
@@ -55,6 +159,17 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 		if (key.equals(KEY_PREF_SYNC_FREQ)) {
 			ListPreference syncFreqPref = (ListPreference) findPreference(key);
 			syncFreqPref.setSummary(syncFreqPref.getEntry());
+			String newValue = syncFreqPref.getValue();
+			if (newValue.equals("0")) {
+				// TODO choose PUSH
+			} else {
+				// choose schedule sync
+				Account account = new Account(VelloConfig.TRELLO_DEFAULT_ACCOUNT_NAME, Constants.ACCOUNT_TYPE);
+				Bundle extras = new Bundle();
+				ContentResolver.removePeriodicSync(account, VelloProvider.AUTHORITY, extras);
+				int pollFrequency = Integer.valueOf(newValue) * 60 * 60;
+				ContentResolver.addPeriodicSync(account, VelloProvider.AUTHORITY, extras, pollFrequency);
+			}
 		}
 	}
 }
